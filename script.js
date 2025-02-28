21269fd2f619b7aba28528d553e0267820709ed6
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-28 23:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+let browserInfo = document.getElementById("browser-info");
+browserInfo.innerHTML = 'You are using ' + navigator.appName + ' version ' + navigator.appVersion;
